c24b767e42faa7a1e4798231496cc7565d0fa1d8
Gaurav Gahlot <gauravgah@cybage.com>
2019-02-05 10:37:36 +0530

    separated scripts for k8s

---
+++ kubernetes/etcd-config-files.sh
@@ -0,0 +1,33 @@
+# Update HOST0, HOST1, and HOST2 with the IPs or resolvable names of your hosts
+export HOST0=x.x.x.x
+export HOST1=x.x.x.x
+export HOST2=x.x.x.x
+
+# Create temp directories to store files that will end up on other hosts.
+mkdir -p /tmp/${HOST0}/ /tmp/${HOST1}/ /tmp/${HOST2}/
+
+ETCDHOSTS=(${HOST0} ${HOST1} ${HOST2})
+NAMES=("k8s-etcd1" "k8s-etcd2" "k8s-etcd3")
+
+for i in "${!ETCDHOSTS[@]}"; do
+HOST=${ETCDHOSTS[$i]}
+NAME=${NAMES[$i]}
+cat << EOF > /tmp/${HOST}/kubeadmcfg.yaml
+apiVersion: "kubeadm.k8s.io/v1beta1"
+kind: ClusterConfiguration
+etcd:
+    local:
+        serverCertSANs:
+        - "${HOST}"
+        peerCertSANs:
+        - "${HOST}"
+        extraArgs:
+            initial-cluster: ${NAMES[0]}=https://${ETCDHOSTS[0]}:2380,${NAMES[1]}=https://${ETCDHOSTS[1]}:2380,${NAMES[2]}$
+            initial-cluster-state: new
+            name: ${NAME}
+            listen-peer-urls: https://${HOST}:2380
+            listen-client-urls: https://${HOST}:2379
+            advertise-client-urls: https://${HOST}:2379
+            initial-advertise-peer-urls: https://${HOST}:2380
+EOF
+done
