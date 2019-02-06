3c771e9dc0e663fb38e560e8ea8154a042ad425c
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2019-02-06 08:18:14 +0530

    combined the two scripts for generating certificates

--- kubernetes/generate-certificates.sh
+++ kubernetes/generate-certificates.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
-# run this script only after the 'etcd-config-files.sh'
+# Set HOST0, HOST1, and HOST2 with the IPs or resolvable names of your hosts
+# before executing this script
+
+
+# Create temp directories to store files that will end up on other hosts.
+mkdir -p /tmp/${HOST0}/ /tmp/${HOST1}/ /tmp/${HOST2}/
+
+ETCDHOSTS=(${HOST0} ${HOST1} ${HOST2})
+NAMES=("k8s-etcd1" "k8s-etcd2" "k8s-etcd3")
+
+for i in "${!ETCDHOSTS[@]}"; do
+HOST=${ETCDHOSTS[$i]}
+NAME=${NAMES[$i]}
+cat << EOF > /tmp/${HOST}/kubeadmcfg.yaml
+apiVersion: "kubeadm.k8s.io/v1beta1"
+kind: ClusterConfiguration
+etcd:
+    local:
+        serverCertSANs:
+        - "${HOST}"
+        peerCertSANs:
+        - "${HOST}"
+        extraArgs:
+            initial-cluster: ${NAMES[0]}=https://${ETCDHOSTS[0]}:2380,${NAMES[1]}=https://${ETCDHOSTS[1]}:2380,${NAMES[2]}$
+            initial-cluster-state: new
+            name: ${NAME}
+            listen-peer-urls: https://${HOST}:2380
+            listen-client-urls: https://${HOST}:2379
+            advertise-client-urls: https://${HOST}:2379
+            initial-advertise-peer-urls: https://${HOST}:2380
+EOF
+done
+
 
 # if you do not already have a CA then run this command on $HOST0 
 # (where you generated the configuration files for kubeadm).
